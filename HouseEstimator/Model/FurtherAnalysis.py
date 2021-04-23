59beb3ce725b74ee882126a7c7db21ca5ad08c59
Ethan353 <ehsanjalali353@mehr.ui.ac.ir>
2021-04-23 18:01:44 +0430

    add more analysis

--- HouseEstimator/Model/FurtherAnalysis.py
+++ HouseEstimator/Model/FurtherAnalysis.py
@@ -7,6 +7,8 @@ df_house.columns = ['link', 'location', 'area', 'room', 'year', 'price']
 
 
 room_number = sorted(set(df_house['room'][df_house['room'] < 8]))
+tenyears = sorted(set(df_house['year'][df_house['year'] % 4 == 0]))
+allyears = sorted(set(df_house['year'][(1370 < df_house['year']) & (df_house['year'] < 1400)]))
 
 # prices = [list((df_house['price'][df_house['room'] == _class]/100000000000)) for _class in room_number]
 # plt.figure()
@@ -22,6 +24,14 @@ room_number = sorted(set(df_house['room'][df_house['room'] < 8]))
 # plt.ylabel("Area")
 # plt.show()
 
+
+# prices = [list((df_house['price'][(df_house['year'] == _class)
+#                                   & (df_house["room"] < 8)]/100000000000)) for _class in tenyears]
+# plt.figure()
+# plt.title('Boxplot of Price base on year')
+# plt.boxplot(prices, labels=tenyears)
+# plt.ylabel("price")
+# plt.show()
 
 # plt.figure()
 # plt.title("Hist of year")
@@ -40,14 +50,53 @@ room_number = sorted(set(df_house['room'][df_house['room'] < 8]))
 
 # plt.figure()
 # plt.title("area VS price")
-# plt.scatter(df_house["area"], df_house["price"])
+# plt.scatter(df_house["area"][df_house["room"] < 8], df_house["price"][df_house["room"] < 8])
 # plt.xlabel("area")
 # plt.ylabel("price")
 # plt.show()
 
 # plt.figure()
 # plt.title("year VS price")
-# plt.scatter(df_house["year"], df_house["price"], color='r')
+# plt.scatter(df_house["year"][df_house["room"] < 8], df_house["price"][df_house["room"] < 8], color='r')
 # plt.xlabel("year")
 # plt.ylabel("price")
+# plt.show()
+
+
+# pricelist = [((df_house['price'][(df_house['year'] == _class) & (df_house["room"] < 8)].max()
+#                - (df_house['price'][(df_house['year'] == _class) & (df_house["room"] < 8)].min()), _class))
+#              for _class in allyears]
+#
+#
+# pricesince = []
+# yearsince = []
+#
+# for _tuple in pricelist:
+#     pricesince.append(_tuple[0])
+#     yearsince.append(_tuple[1])
+#
+# plt.figure()
+# plt.title("price changes since 1370 with their (max - min)")
+# plt.plot(yearsince, pricesince)
+# plt.xlabel('year')
+# plt.ylabel('price')
+# plt.show()
+
+
+# pricelist2 = [(df_house['price'][(df_house['year'] == _class)
+#                                  & (df_house["room"] < 8)].mean()/1000000000, _class) for _class in allyears]
+#
+#
+# pricesince = []
+# yearsince = []
+#
+# for _tuple in pricelist2:
+#     pricesince.append(_tuple[0])
+#     yearsince.append(_tuple[1])
+#
+# plt.figure()
+# plt.title("price changes since 1370 with their average")
+# plt.plot(yearsince, pricesince, c='g')
+# plt.xlabel('year')
+# plt.ylabel('for real price multiply to 1000000000')
 # plt.show()
