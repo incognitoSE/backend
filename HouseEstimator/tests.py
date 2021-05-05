f944617a02aa2c0dd3ac98772f6e6d7ed8fc0244
Ethan353 <ehsanjalali353@mehr.ui.ac.ir>
2021-05-05 18:59:24 +0430

    tests completed

--- HouseEstimator/tests.py
+++ HouseEstimator/tests.py
@@ -10,7 +10,7 @@ from .serializers import HouseSerializer
 
 class HouseEstimatorTest(APITestCase):
     def setUp(self):
-        self.valid = {"area": 120, "location": ' فرشته', "room": 2, "year": 1390}
+        self.valid = {"area": 120, "room": 2, "year": 1390, "location": 'فرشته'}
         self.invalid = {"area": "", "location": ' فرشته', "room": 2, "year": 1390}
         self.invalid2 = {"area": 120, "location": ' فرشته', "room": 2}
 
@@ -28,6 +28,24 @@ class HouseEstimatorTest(APITestCase):
         response = self.client.post("/HEstimator/House/",
                                     data=json.dumps(self.invalid2), content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_price_data(self):
+        price = self.client.post("/HEstimator/House/", data=self.valid).data.get("price")
+        self.assertIsInstance(price, int)
+
+    def test_houses_data(self):
+        houses = self.client.post("/HEstimator/House/", data=self.valid).data.get("houses")
+        self.assertIsInstance(houses, list)
+
+    def test_current_data(self):
+        room = self.client.post("/HEstimator/House/", data=self.valid).data.get("currenthouse").get("room")
+        location = self.client.post("/HEstimator/House/", data=self.valid).data.get("currenthouse").get("location")
+        area = self.client.post("/HEstimator/House/", data=self.valid).data.get("currenthouse").get("area")
+        year = self.client.post("/HEstimator/House/", data=self.valid).data.get("currenthouse").get("year")
+        self.assertEqual(room, self.valid.get("room"))
+        self.assertEqual(year, self.valid.get("year"))
+        self.assertEqual(location, self.valid.get("location"))
+        self.assertEqual(area, self.valid.get("area"))
 
     def test_main_page(self):
         response = self.client.get("/HEstimator/House/")
