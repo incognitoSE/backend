13ed52c06c3069e66344eef3ec5cdb90fb1e5f79
AmirHossein <58815779+safdarian@users.noreply.github.com>
2021-05-20 01:28:04 +0430

    refresh token time changed to 5 minutes

--- MachineLearningEstimator/settings.py
+++ MachineLearningEstimator/settings.py
@@ -161,7 +161,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=2),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=5),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
 
